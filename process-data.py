4f29a0382ad56c2254a9c9cb551c671b04324c22
GBDM97 <ghian.m@gmail.com>
2022-10-23 20:22:15 -0300

    getting salaries

--- process-data.py
+++ process-data.py
@@ -1,1 +1,24 @@
 import sqlite3
+
+conn = sqlite3.connect('teleport-db.sqlite')
+cur = conn.cursor()
+aCount = 1
+
+cur.execute('SELECT * FROM AdminDs ORDER BY id DESC LIMIT 1')
+aCountLimit = cur.fetchone()[0]
+
+while True:
+    cur.execute('SELECT salary FROM Cities WHERE id_adminD = (?)', (aCount, ))
+    unknown = cur.fetchone()[0]
+    aCount = aCount + 1
+    if aCount > aCountLimit: break
+
+    print(unknown,type(unknown))
+
+    # salary DECIMAL UNIQUE,
+    # leisureS DECIMAL UNIQUE,
+    # safetyS DECIMAL UNIQUE,
+    # businessFredomS DECIMAL UNIQUE,
+    # costOfLivingS DECIMAL UNIQUE,
+    # travelConnectivityS DECIMAL UNIQUE,
+    # educationS DECIMAL UNIQUE,

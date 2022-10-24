f9f3384b6c14976125fe035bb366904aadb3dd9c
GBDM97 <ghian.m@gmail.com>
2022-10-24 02:50:41 -0300

    multiple bug fixes

--- process-data.py
+++ process-data.py
@@ -2,58 +2,47 @@ import sqlite3
 
 conn = sqlite3.connect('teleport-db.sqlite')
 cur = conn.cursor()
-line = 1
+line = 0
 aCount = 1
-count = 1
-salary = 0
-leisureS = 0 
-safetyS = 0 
-businessFredomS = 0 
-costOfLivingS = 0 
-travelConnectivityS = 0 
-educationS = 0 
-aCountLimit = cur.execute('''SELECT * FROM adminDs ORDER BY ID DESC LIMIT 1''')
+cur.execute('''SELECT id FROM adminDs ORDER BY ID DESC LIMIT 1''')
+out = cur.fetchone()
+aCountLimit = int(out[0])
+l = [0,0,0,0,0,0,0,0]
 
 while True:
     while True:
+        line = line + 1
         try:
             cur.execute('SELECT id_adminD,salary,leisureS,safetyS,businessFredomS,costOfLivingS,travelConnectivityS,educationS FROM Cities WHERE id = (?)', (line, ))
-            out = cur.fetchone()
-            data = str(out)
-            data = data.strip(')').strip('(').split(',')
-            print(type(data))
-            id = data[0]
-            num = data[1]
+            data = cur.fetchone()
+            id = int(data[0])
             if id == aCount:
-                try:
-                    l[0] = l[0] + 1 
-                    l[1] = l[1] + data[1]
-                    l[2] = l[2] + data[2]
-                    l[3] = l[3] + data[3]
-                    l[4] = l[4] + data[4]
-                    l[5] = l[5] + data[5]
-                    l[6] = l[6] + data[6]
-                    l[7] = l[7] + data[7]
-                except:
-                    l = [ count , salary , leisureS , safetyS , businessFredomS , costOfLivingS , travelConnectivityS , educationS ] 
-            count = count + 1 
+                l[0] = l[0] + 1 
+                l[1] = l[1] + data[1]
+                l[2] = l[2] + data[2]
+                l[3] = l[3] + data[3]
+                l[4] = l[4] + data[4]
+                l[5] = l[5] + data[5]
+                l[6] = l[6] + data[6]
+                l[7] = l[7] + data[7]
         except Exception as err: print(err); break
-
-        listCount = l[0]
-        l[0] = l[1] / listCount
-        l[1] = l[2] / listCount
-        l[2] = l[3] / listCount
-        l[3] = l[4] / listCount
-        l[4] = l[5] / listCount
-        l[5] = l[6] / listCount
-        l[6] = l[7] / listCount
-        l[7] = l[8] / listCount
-
-        cur.execute('''INSERT INTO adminDs (salary , leisureS , safetyS , businessFredomS , costOfLivingS , travelConnectivityS , educationS) VALUES (?,?,?,?,?,?,?)''', (salary , leisureS , safetyS , businessFredomS , costOfLivingS , travelConnectivityS , educationS) )
-
+        
+    listCount = l[0]
+    if listCount == 0: listCount = 1
+    l[0] = l[1] / listCount
+    l[1] = l[2] / listCount
+    l[2] = l[3] / listCount
+    l[3] = l[4] / listCount
+    l[4] = l[5] / listCount
+    l[5] = l[6] / listCount
+    l[6] = l[7] / listCount
+    l[7] = 0
+    cur.execute('''INSERT INTO adminDs (mediumSalary , leisureS , safetyS , businessFredomS , costOfLivingS , travelConnectivityS , educationS) VALUES (?,?,?,?,?,?,?) WHERE id = (?)''', (l[0] , l[1] , l[2] , l[3] , l[4] , l[5] , l[6])(aCount) )
+    l = [0,0,0,0,0,0,0,0]
+    line = 0
     aCount = aCount + 1
     if aCount > aCountLimit: break
-
+conn.commit()
 
     # salary DECIMAL UNIQUE,
     # leisureS DECIMAL UNIQUE,
